db6b0f1b37fa4e24104bdc71f7d6ce9478dc0562
shu-36 <syu7222@gmail.com>
2021-04-20 18:11:46 +0900

    migrate commit

---
+++ database/migrations/2021_04_20_081013_create_favorites_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //unsignedで正の数を指定
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('favorite_id')->unsigned();
+            //foreignで今のマイグレーション、referencesで参照したいものを決め、onでどのテーブルを参照するか
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('favorite_id')->references('id')->on('posts');
+            $table->softDeletes();
+            $table->timestamps();
+            
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
