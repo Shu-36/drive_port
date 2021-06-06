d5a4da231fd68cfb535771dfaf9942282840eaad
shu-36 <syu7222@gmail.com>
2021-06-06 17:05:32 +0900

    varcher191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         \URL::forceScheme('https');
         $this->app['request']->server->set('HTTPS','on');
+        Schema::defaultStringLength(191);
     }
 }
